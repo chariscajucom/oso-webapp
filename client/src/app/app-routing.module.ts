2a242a01ae9de592c405b4eeaf4e258728148f0c
Charis Cajucom <cajucom@jcharis@gmail.com>
2018-05-02 23:58:22 +0800

    added news in leader dash

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ export const AppRoutes: Routes = [
       children: [
         { path: '', loadChildren: './studleader/dashboard/dashboard.module#DashboardModule'},
         { path: 'dashboard', loadChildren: './studleader/dashboard/dashboard.module#DashboardModule'},
-        // { path: 'news', loadChildren: './studleader/news/news.module#NewsModule'},
+        { path: 'news', loadChildren: './studleader/news/news.module#NewsModule'},
         { path: 'activities', loadChildren: './studleader/activities/activities.module#ActivitiesModule'},
         // { path: 'pdf-forms', loadChildren: './studleader/pdf-forms/pdforms.module#PDFModule'},
       ]
